cdf613328d974647df172916f22cc09369afbef5
shroomeater <shroomeater@gmail.com>
2017-03-10 20:13:31 +0100

    Create 28_RFID_escribir_y_leer_tarjeta.ino

---
+++ 28_RFID_leer_y_escribir_tarjeta/28_RFID_escribir_y_leer_tarjeta.ino
@@ -0,0 +1,75 @@
+#include <SPI.h>
+#include <MFRC522.h>
+const int RST_PIN = 9;
+const int SS_PIN = 10;
+unsigned char data[16] = { 'T','E','S','T',' ','R','F','I','D',' ','M','F','R', '5','5','2'};
+unsigned char *writeData = data;
+unsigned char *str;
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+
+void printArray(byte *buffer, byte bufferSize) {
+for (byte i = 0; i < bufferSize; i++) {
+Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+Serial.print(buffer[i], HEX);
+}
+}
+void setup()
+{ Serial.begin(9600);
+SPI.begin();
+mfrc522.PCD_Init();
+for (byte i = 0; i < 6; i++) {
+key.keyByte[i] = 0xFF;
+}
+}
+
+void loop()
+{
+if (!mfrc522.PICC_IsNewCardPresent()) return;
+if (!mfrc522.PICC_ReadCardSerial()) return;
+MFRC522::StatusCode status;
+byte trailerBlock = 7;
+byte sector = 1;
+byte blockAddr = 4;
+status = (MFRC522::StatusCode)
+mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,
+trailerBlock, &key, &(mfrc522.uid));
+if (status != MFRC522::STATUS_OK) {
+Serial.print(F("PCD_Authenticate() failed: "));
+Serial.println(mfrc522.GetStatusCodeName(status));
+return;
+} //continua en la siguiente diapositiva...
+
+Serial.print(F("Escribir datos en sector "));
+Serial.print(blockAddr);
+Serial.println(F(" ..."));
+printArray((byte*)data, 16); Serial.println();
+status = (MFRC522::StatusCode)
+mfrc522.MIFARE_Write(blockAddr, (byte*)data, 16);
+if (status != MFRC522::STATUS_OK) {
+Serial.print(F("Error en MIFARE_Write() : "));
+Serial.println(mfrc522.GetStatusCodeName(status));
+}
+Serial.println();
+byte buffer[18];
+byte size = sizeof(buffer);
+//continua en la siguiente diapositiva...
+
+// Lee datos
+Serial.print(F("Leer datos del sector "));
+Serial.print(blockAddr);
+Serial.println(F(" ..."));
+status = (MFRC522::StatusCode)
+mfrc522.MIFARE_Read(blockAddr, buffer, &size);
+if (status != MFRC522::STATUS_OK) {
+Serial.print(F("Error en MIFARE_Read(): "));
+Serial.println(mfrc522.GetStatusCodeName(status));
+}
+Serial.print(F("Datos en el bloque "));
+Serial.print(blockAddr); Serial.println(F(":"));
+printArray(buffer, 16); Serial.println();
+// Halt PICC
+mfrc522.PICC_HaltA();
+// Stop encryption on PCD
+mfrc522.PCD_StopCrypto1();
+}
